dd2c2deeb83f79a703beefdc82bb3644a1b34b33
Aaron Sutula <asutula@users.noreply.github.com>
2024-09-12 10:35:13 -0600

    add cookie config to hopefully fix oauth callback error
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- src/auth.ts
+++ src/auth.ts
@@ -3,4 +3,15 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  cookies: {
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
 });
