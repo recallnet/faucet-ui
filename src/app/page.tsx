2d939b4846add4bcb2bc9d17652f65b7a022dc0e
Aaron Sutula <asutula@users.noreply.github.com>
2024-09-11 18:48:10 -0600

    padding
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const session = await auth();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gradient-to-bl from-gray-200 via-fuchsia-200 to-stone-100">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gradient-to-bl from-gray-200 via-fuchsia-200 to-stone-100 p-4">
       <div className="flex flex-col items-center">
         <span className="text-4xl font-bold">Hoku Network</span>
         <span className="text-sm font-medium text-muted-foreground">
