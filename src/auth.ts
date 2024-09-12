834118e1719bf84c2d7aecc5f7084e87e0f4e9c4
Aaron Sutula <asutula@users.noreply.github.com>
2024-09-12 10:46:10 -0600

    another cookie config
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,15 @@ import GitHub from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   cookies: {
+    csrfToken: {
+      name: "next-auth.csrf-token",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
     pkceCodeVerifier: {
       name: "next-auth.pkce.code_verifier",
       options: {
